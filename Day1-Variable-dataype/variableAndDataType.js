cea9f5161e31513757076cda060f1dc15d212813
Ankush Kumar Kushwaha <ankush.k@navankur.org>
2024-07-14 01:30:33 +0530

    Day-1

---
+++ Day1-Variable-dataype/variableAndDataType.js
@@ -0,0 +1,29 @@
+var num1=1;
+console.log(num1);
+let str="Ankush";
+console.log(str);
+
+const bool=true;
+console.log(bool);
+
+const num = new Number(10);
+console.log( typeof num);
+
+const string = new String("New Ankush");
+console.log(typeof string);
+
+const boolean = new Boolean(true);
+console.log(typeof boolean);
+
+const obj = new Object();
+console.log(typeof obj);
+
+const arr = new Array();
+console.log(typeof Array);
+
+let newVal=99;
+console.log(newVal);
+newVal=100;
+console.log(newVal);
+
+//bool = false; error
